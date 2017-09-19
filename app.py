29e3702efef6b0787fe9e72459418ad9267c5b0f
Michal <kubiakmichal@ymail.com>
2017-09-19 23:00:46 +0200

    Added first view template

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Item, Category, User
 
 app = Flask(__name__)
+engine  = create_engine('sqlite:///itemsapp.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+def mainView(restaurant_id):
+    return render_template('main.html')
